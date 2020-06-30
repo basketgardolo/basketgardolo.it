f6fadd24e45250b41cac58cf9bd89a05012212f4
Cristian Stenico <cristianstenico@gmail.com>
2020-07-01 00:03:39 +0200

    ariprova

--- functions/notify_slack/notify_slack.go
+++ functions/notify_slack/notify_slack.go
@@ -2,52 +2,43 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	log.Printf("EVENT: %s", request.Body)
-	lc, ok := lambdacontext.FromContext(ctx)
-	if ok {
-		cc := lc.ClientContext
-		requestBody, err := json.Marshal(map[string]string{
-			"text": "Tutto ok",
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Printf("Environment: %s", cc.Env["SLACK_ENDPOINT"])
-		resp, err := http.Post(cc.Env["SLACK_ENDPOINT"], "application/json", bytes.NewBuffer(requestBody))
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		log.Println((string(body)))
-
-		return &events.APIGatewayProxyResponse{
-			StatusCode: resp.StatusCode,
-			Body:       string(body),
-		}, nil
+	requestBody, err := json.Marshal(map[string]string{
+		"text": "Tutto ok",
+	})
+	if err != nil {
+		log.Fatalln(err)
 	}
+	log.Printf("Environment: %s", os.Getenv("SLACK_ENDPOINT"))
+	resp, err := http.Post(os.Getenv("SLACK_ENDPOINT"), "application/json", bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println((string(body)))
+
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 500,
-		Body:       "Hello world",
+		StatusCode: resp.StatusCode,
+		Body:       string(body),
 	}, nil
 }
 
